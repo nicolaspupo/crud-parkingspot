dfdaffc0f7b015daa410c60f2dbaca4077dcdaff
Nicolas Pupo <nicolaspwpo@gmail.com>
2024-08-13 17:45:05 -0300

    Primeiro commit

---
+++ parking-control/src/main/java/com/api/parking_control/ParkingControlApplication.java
@@ -0,0 +1,17 @@
+package com.api.parking_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SuppressWarnings("unused")
+@SpringBootApplication
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {SpringApplication.run(ParkingControlApplication.class, args);}
+
+	}
+
